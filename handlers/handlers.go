31d92b22cc23b32475556acaeeb510fa9fd71186
Gubin <gubin.sm@micran.ru>
2025-11-13 03:48:01 +0700

    Написал models.go, docker-compose и протестировал работоспособность

---
+++ handlers/handlers.go
@@ -0,0 +1,9 @@
+// package handlers
+
+// import (
+// 	"errors"
+// )
+
+// var errTeamExist = errors.New("team_name already exists")
+// var notFound = errors.New("resource not found")
+// var PrExist = errors.New("PR id already exists ")
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"avito_intern_dev/models"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "host = db user=postgres password=pass dbname=prdb port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Could not connect to DB", err)
+	}
+	db.AutoMigrate(&models.Team{}, &models.User{}, &models.PullRequest{})
+
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+	r.Run(":8080")
+}
